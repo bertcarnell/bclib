03071895c105729f5c109f08abbd6caf18ae0593
Rob Carnell <bertcarnell@gmail.com>
2020-10-29 14:33:58 -0400

    Updated with namespace version string that is entered by CMAKE

--- bclibtest/VersionTest.cpp
+++ bclibtest/VersionTest.cpp
@@ -1,42 +1,38 @@
 /**
- * @file main.cpp
+ * @file VersionTest.cpp
  * @author Robert Carnell
- * @copyright Copyright (c) 2013, Robert Carnell
- * 
+ * @copyright Copyright (c) 2020, Robert Carnell
+ *
  * @license <a href="http://www.gnu.org/licenses/gpl.html">GNU General Public License (GPL v3)</a>
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <cstdlib>
-#include <cstdio>
-#include <vector>
-#include "TestClass.h"
-#include "matrixTest.h"
-#include "simpleAssertTest.h"
-#include "orderTest.h"
-#include "RWarningExceptionTest.h"
 #include "VersionTest.h"
 
-using namespace bclibtest;
+namespace bclibtest {
 
-int main(int argc, const char* argv[] )
-{
-    START_TESTS("Starting Simple Tests...\n");
-	CREATE_TEST(matrixTest);
-    CREATE_TEST(simpleAssertTest);
-    CREATE_TEST(orderTest);
-    CREATE_TEST(RWarningExceptionTest);
-    CREATE_TEST(VersionTest);
-    EXECUTE_TESTS(NULL);
+    void VersionTest::Run()
+    {
+        printf("\tVersionTest...");
+        testVersion();
+        printf("passed\n");
+    }
+
+    void VersionTest::testVersion()
+    {
+        std::string sversion = std::string(bclib::version);
+        bclib::Assert(!sversion.empty());
+        bclib::Assert(sversion.length() >= 5);
+    }
 }
--- bclibtest/VersionTest.h
+++ bclibtest/VersionTest.h
@@ -1,42 +1,39 @@
 /**
- * @file main.cpp
+ * @file VersionTest.h
  * @author Robert Carnell
- * @copyright Copyright (c) 2013, Robert Carnell
- * 
+ * @copyright Copyright (c) 2020, Robert Carnell
+ *
  * @license <a href="http://www.gnu.org/licenses/gpl.html">GNU General Public License (GPL v3)</a>
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <cstdlib>
-#include <cstdio>
-#include <vector>
+#ifndef VERSIONTEST_H
+#define	VERSIONTEST_H
+
+#include <algorithm>
+#include <numeric>
 #include "TestClass.h"
-#include "matrixTest.h"
-#include "simpleAssertTest.h"
-#include "orderTest.h"
-#include "RWarningExceptionTest.h"
-#include "VersionTest.h"
+#include "simpleAssert.h"
+#include "bclibVersion.h"
 
-using namespace bclibtest;
-
-int main(int argc, const char* argv[] )
+namespace bclibtest
 {
-    START_TESTS("Starting Simple Tests...\n");
-	CREATE_TEST(matrixTest);
-    CREATE_TEST(simpleAssertTest);
-    CREATE_TEST(orderTest);
-    CREATE_TEST(RWarningExceptionTest);
-    CREATE_TEST(VersionTest);
-    EXECUTE_TESTS(NULL);
+    class VersionTest : public bclib::TestClass
+    {
+        void Run();
+        void testVersion();
+    };
 }
+
+#endif
