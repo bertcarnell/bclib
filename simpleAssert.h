f0d57dd38b302aeed98beec47450a633ce5d46cd
Rob Carnell <bertcarnell@gmail.com>
2021-08-30 13:19:51 -0400

    fixed references in catch expressions

--- simpleAssert.h
+++ simpleAssert.h
@@ -35,7 +35,7 @@
     { \
         exp; \
     } \
-    catch (std::exception e) \
+    catch (std::exception const & e) \
     { \
         throw bclib::assertion_error("Failed: an exception was thrown in assert_nothrow"); \
     }
@@ -51,10 +51,10 @@
         /*assertion_error will not be caught by the runtime_error or logic_error*/ \
         throw bclib::assertion_error("Failed: no exception was thrown in assert_throw"); \
     } \
-    catch (std::runtime_error re) \
+    catch (std::runtime_error const& re) \
     { \
     } \
-    catch (std::logic_error le) \
+    catch (std::logic_error const& le) \
     { \
     }
 
@@ -68,7 +68,7 @@
         exp; \
         throw std::runtime_error("Failed: no exception was thrown in assert_noassertionerror"); \
     } \
-    catch (bclib::assertion_error ae) \
+    catch (bclib::assertion_error const& ae) \
     { \
         /* expected */ \
     }
