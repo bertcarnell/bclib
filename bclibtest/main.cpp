a460ccabeb57c7eff171e2205dd28f853ef228d3
Rob Carnell <bertcarnell@gmail.com>
2026-04-15 22:37:05 -0400

    normalized file paths again with better Cmake files

--- bclibtest/main.cpp
+++ bclibtest/main.cpp
@@ -21,7 +21,7 @@
 #include <cstdlib>
 #include <cstdio>
 #include <vector>
-#include "../TestClass.h"
+#include "TestClass.h"
 #include "matrixTest.h"
 #include "simpleAssertTest.h"
 #include "orderTest.h"
